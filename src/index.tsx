4b84dc336d05b188762a38c517562bb6ad328ee8
Oleg <60295799+OlEzhA0@users.noreply.github.com>
2020-07-15 17:38:35 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import { AppContextWrapper } from "./appContext";
